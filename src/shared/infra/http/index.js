1cc6a6ad4ea1fef029ab1e3a542ffd1fe8661f23
henriquepe <henrique.p.espindola@gmail.com>
2021-02-26 12:20:38 -0300

    heroku api

--- src/shared/infra/http/index.js
+++ src/shared/infra/http/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+
 require('../mongoose')
 const cors = require('cors');
-const rootRouter = require('../http/routes/index')
+const rootRouter = require('./routes')
 
 const app = express()
+
+require('dotenv').config()
+app.use(express.json());
 app.use(cors())
-app.use(express.json())
-app.use(rootRouter)
+app.use(rootRouter);
 
 
 const PORT = process.env.PORT || 3000
@@ -17,11 +20,11 @@ app.listen(PORT, () => {
 })
 
 
-// Teste FullStack: Criar um CRUD de clientes. 
+// Teste FullStack: Criar um CRUD de clientes. [x]
 
-// Cliente: Pode ser básico, CPF, Nome, Email, Telefone, etc
+// Cliente: Pode ser básico, CPF, Nome, Email, Telefone, etc [x]
 
-// Backend:  Criar a API utilizando NODE.js.
+// Backend:  Criar a API utilizando NODE.js. [x]
 
 // FRONTEND: Aqui na Qualicorp utilizamos VUE.Js, mas pode ficar à vontade para escolher qual utilizar.
 
--- src/shared/infra/mongoose/index.js
+++ src/shared/infra/mongoose/index.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27018/qualicorp_db';
+let url = "";
+
+console.log(process.env.DATABASE_URL_LOCAL)
+
+if(process.env.NODE_ENV === 'production'){
+    url = process.env.DATABASE_URL_CLOUD;
+}
+else {
+    url = process.env.DATABASE_URL_LOCAL;
+    
+}
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -8,5 +18,5 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => {
-    `banco de dados conectado com sucesso!`
+    console.log(`banco de dados conectado com sucesso!`)
 })
